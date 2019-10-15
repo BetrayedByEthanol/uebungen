3380a7fd74bc315b6f2e5f24200a9bcf0bf6f59d
19behm <ju.behm@gmail.com>
2019-10-15 15:01:38 +0200

    Reformated 2

--- 19behm/CGrundlagen001/Aufgaben/Uhrzeit2Winkel.c
+++ 19behm/CGrundlagen001/Aufgaben/Uhrzeit2Winkel.c
@@ -2,24 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int min(int x, int y) { return (x < y) ? x : y; }
-int calcAngle(double h, double m) 
-{ 
-  
-    if (h == 12||h==24) h = 0; 
-    if (m == 60) m = 0; 
-  
-   
-    int hour_angle = 0.5 * (h*60 + m); 
-    int minute_angle = 6*m; 
-    int angle = abs(hour_angle - minute_angle); 
-    angle = min(360-angle, angle); 
-    if(angle<=0)
-        angle*=(-1);
-  
-    return angle; 
-} 
+int min(int x, int y) {
+    return (x < y) ? x : y; 
+}
+
+int calcAngle(double h, double m)
+{
+
+    if (h == 12 || h == 24)
+        h = 0;
+    if (m == 60)
+        m = 0;
+
+    int hour_angle = 0.5 * (h * 60 + m);
+    int minute_angle = 6 * m;
+    int angle = abs(hour_angle - minute_angle);
+    angle = min(360 - angle, angle);
+    if (angle <= 0)
+        angle *= (-1);
+
+    return angle;
+}
+
 int h, m;
+
 int main()
 {
 
@@ -31,7 +37,7 @@ int main()
     }
     else
     {
-        printf("%d \n", calcAngle(h, m));
+        printf("Der Winkel zwischen den Zeigern bertägt %d Grad.\n", calcAngle(h, m));
     }
     return 0;
 }
