86513c28c1fdc6dbe087648a4e79be11270babac
19behm <ju.behm@gmail.com>
2019-11-12 15:17:37 +0100

    Aufgabe CGrundlagenA003-C commented

--- 19behm/CGrundlagenA003/CGrundlagenA003.c
+++ 19behm/CGrundlagenA003/CGrundlagenA003.c
@@ -4,22 +4,21 @@
 #include <string.h>
 #include <ctype.h>
 
-char first[10][10], second[10][10], third[10][10];
+char first[10][10], second[10][10], third[10][10]; //GLobale Variablen für spätere Ausgabe
 
-
-char definition(char xp, char yp[][10])
-{
-char zero[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ "#   #"}, {"#   #"}, {"#   #"}, {" ### "}};
-char one[10][10] = {{"  #  "},{ " ##  "},{ "  #  "},{ "  #  "}, {"  #  "}, {"  #  "}, {" ### "}};
-char two[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ "   # "}, {"  #  "}, {" #   "}, {"#####"}};
-char three[10][10] = {{" ### "},{ "#   #"},{ "    #"},{ " ### "}, {"    #"}, {"#   #"}, {" ### "}};
-char four[10][10] = {{"   # "},{ "  ## "},{ " # # "},{ "#  # "}, {"#####"}, {"   # "}, {"   # "}};
-char five[10][10] = {{"#####"},{ "#    "},{ "#    "},{ "#### "},{ "    #"}, {"#   #"}, {" ### "}};
-char six[10][10] = {{" ### "},{ "#   #"},{ "#    "},{ "#### "},{ "#   #"}, {"#   #"}, {" ### "}};
-char seven[10][10] = {{"#####"},{ "    #"},{ "    #"},{ "   # "},{ "  #  "},{ " #   "},{ "#    "}};
-char eight[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ " ### "},{ "#   #"},{ "#   #"},{ " ### "}};
-char nine[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ " ####"},{ "    #"},{ "#   #"},{ " ### "}};
-
+void definition(char xp, char yp[10][10]) //Funktion zum erstellen der Ausgabe
+{   //2D-Arrays welche die Zahlen in Zeichenfolge beinhalten
+    char zero[10][10] = {{" ### "}, {"#   #"}, {"#   #"}, {"#   #"}, {"#   #"}, {"#   #"}, {" ### "}};
+    char one[10][10] = {{"  #  "}, {" ##  "}, {"  #  "}, {"  #  "}, {"  #  "}, {"  #  "}, {" ### "}};
+    char two[10][10] = {{" ### "}, {"#   #"}, {"#   #"}, {"   # "}, {"  #  "}, {" #   "}, {"#####"}};
+    char three[10][10] = {{" ### "}, {"#   #"}, {"    #"}, {" ### "}, {"    #"}, {"#   #"}, {" ### "}};
+    char four[10][10] = {{"   # "}, {"  ## "}, {" # # "}, {"#  # "}, {"#####"}, {"   # "}, {"   # "}};
+    char five[10][10] = {{"#####"}, {"#    "}, {"#    "}, {"#### "}, {"    #"}, {"#   #"}, {" ### "}};
+    char six[10][10] = {{" ### "}, {"#   #"}, {"#    "}, {"#### "}, {"#   #"}, {"#   #"}, {" ### "}};
+    char seven[10][10] = {{"#####"}, {"    #"}, {"    #"}, {"   # "}, {"  #  "}, {" #   "}, {"#    "}};
+    char eight[10][10] = {{" ### "}, {"#   #"}, {"#   #"}, {" ### "}, {"#   #"}, {"#   #"}, {" ### "}};
+    char nine[10][10] = {{" ### "}, {"#   #"}, {"#   #"}, {" ####"}, {"    #"}, {"#   #"}, {" ### "}};
+    //Vergleich der Eingabe und Generierung der Ausgabe
     if (xp == '0')
         yp = zero;
     else if (xp == '1')
@@ -27,7 +26,7 @@ char nine[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ " ####"},{ "    #"},{ "# 
     else if (xp == '2')
         yp = two;
     else if (xp == '3')
-       yp = three;
+        yp = three;
     else if (xp == '4')
         yp = four;
     else if (xp == '5')
@@ -45,23 +44,22 @@ char nine[10][10] = {{" ### "},{ "#   #"},{ "#   #"},{ " ####"},{ "    #"},{ "# 
 int main()
 {
     char input[10];
-    
 
-    printf("Bitte geben Sie die zu zeichnende Zahl ein.\n");
-    scanf("%s", &input);
-    char inp1 = input[0];
+    printf("Bitte geben Sie die zu zeichnende Zahl ein.\n"); //Abfrage der Eingabe
+    scanf("%s", &input); //Hinzufügen zum Array
+    char inp1 = input[0]; //Auslesen der Arrayzeichen und Speicherung in Variablen
     char inp2 = input[1];
     char inp3 = input[2];
-
+    //Ausführung der oberen Funktion 3-mal mit jedem Eingabewert / für jeden Ausgabewert
     definition(inp1, first);
     definition(inp2, second);
     definition(inp3, third);
 
-    for(int i = 0; i < 8; i++)
+    for (int i = 0; i < 8; i++) //For-Schleife zum Erstellen des Zeichenfeldes
     {
         printf("%s\t%s\t%s\n", first[i], second[i], third[i]);
-        /*printf("%s\n", first[i]);
-        puts("Test");*/
+        //printf("%s\n", first[i]);
+        //puts("Test");
     }
 
     return 0;
