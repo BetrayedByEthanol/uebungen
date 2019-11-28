0def1b06dc8e26f40107288573fc69d9db5ad4bf
Nils <Doodlegante@mehrfabigerhalbkreisfoermigerbogen.de>
2019-11-28 12:59:18 +0100

    Experimentelle Daten wurden gesichert, Nils Rose

---
+++ 19rose/WiP/test.js
@@ -0,0 +1,11 @@
+process.stdin.once('data',(toll) => {
+    console.log(toll.toString());
+    
+    process.exit()
+    
+})
+process.stdin.once('data',(tall) => {
+    console.log(tall.toString());
+    
+    process.exit()
+})
