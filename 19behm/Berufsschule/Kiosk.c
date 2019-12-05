712eb37c0d57fab22ab37908d229caade63023d5
19behm <ju.behm@gmail.com>
2019-12-05 15:40:21 +0100

    Added Html5

--- 19behm/Berufsschule/Kiosk.c
+++ 19behm/Berufsschule/Kiosk.c
@@ -1,5 +1,5 @@
 
-#include <stdlib>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,52 +9,72 @@
 //sicherheitesabfrage nicht negativ...
 //tabellen: table_header, table,article
 
-String table_header[5];
-double table[5][5];
-String article[5];
+char table_header[6][16] = {"Art-Nr.","Artikel", "Bestand", "Preis", "Verkauft", "Umsatz"};
+double table[6][5];
+char article[6][16] = {"Radiergummi", "Schreibblock", "Aktenordner", "Taschenrechner", "Bleistift"};
 int input;
 
-void inittable() 
-{
-    table_header = {"Artikel","Bestand","Preis","Verkauft","Umsatz"}
-    for(int i = 0; i<5;i++)
-        table[0][i] = i;
-    table[] = 0;
-    article = {"Radiergummi","Schreibblock","Aktenordner","Taschenrechner","Bleistift"};
-}
-
-void restock(int artnr, int stkzahl)
-{
-    table[0][artnr] += stkzahl;
-}
-
-void sell(int artnr,int stkzahl)
+void inittable()
 {
     
+    for (int i = 0; i < 5; i++){
+        for(int j =0;j<5;j++)
+            table[j][i] = 0;
+    }
+            
+    for(int i=0; i<5;i++)
+        table[0][i] = i;
+    
+}
+
+void restock()
+{
+    int artnr, stkzahl;
+    printf("Bitte Artikelnummer eingeben\n");
+    scanf("%d", &artnr);
+    printf("Wie viele %s moechten Sie hinzufügen?\n",table[1][artnr]);
+    scanf(" %d", &stkzahl);
+    table[1][artnr] += stkzahl;
+}
+
+void sell()
+{
+    int artnr, stkzahl;
+    double preis;
+    printf("Bitte Artikelnummer eingeben\n");
+    scanf(" %d",&artnr);
+    printf("Wieviele sollen verkauft werden?\n");
+    scanf(" %d",&stkzahl);
+    preis = table[3][artnr] * stkzahl;
+    printf("Der Preis betraegt: %.2f", preis);
+    table[2][artnr] -= stkzahl;
+    table[4][artnr] += stkzahl;
+    table[5][artnr] = table[3][artnr] * stkzahl;
 }
 
 void output();
 
 void outputall();
 
-int main() 
+int main()
 {
     inittable();
     
-    while(1)
+
+    while (1)
     {
         printf("Bitte Option waehlen. \n [1] Verkaufen \n [2] Bestand auffuellen \n [3] Tabelle ausgeben \n [4] Gesamtumsatz");
-        scanf(" %d",&input);
-        
-        if(input == 1)
-            sell();
-        else if(input == 2)
-            restock();
-        else if(input == 3)
-            output();
-        else if(input == 4)
-            outputall();
-        else printf("Falsche Eingabe");
-    }
+        scanf(" %d", &input);
 
+        if (input == 1)
+            sell();
+        else if (input == 2)
+            restock();
+        else if (input == 3)
+            output();
+        else if (input == 4)
+            outputall();
+        else
+            printf("Falsche Eingabe");
+    }
 }
