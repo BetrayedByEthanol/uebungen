582036bcc83654437bf0bb39a115990124aae8e3
Denis Heimburger <d.heimburger@albbw.de>
2019-12-13 12:37:56 +0100

    #WebGrundlagenB003: added comment

--- web/htmlBasics/WebGrundlagenb001/app.js
+++ web/htmlBasics/WebGrundlagenb001/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 5050;
-
+//Static file server
 app.use(express.static('src'));
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
