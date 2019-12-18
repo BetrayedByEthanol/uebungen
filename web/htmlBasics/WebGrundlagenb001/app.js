fe011615c759040591c6e6a9e95337e327d9dbb6
Denis Heimburger <d.heimburger@albbw.de>
2019-12-18 11:17:59 +0100

    #WebGrundlagenB003: implemented 404 page

--- web/htmlBasics/WebGrundlagenb001/app.js
+++ web/htmlBasics/WebGrundlagenb001/app.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
+var path = require("path");
 const app = express();
 const port = 5050;
 //Static file server bla bla
-app.use(express.static('src'));
-  
+app.use(express.static("src"));
+app.set("view engine", "pug");
+
+app.get('/404.html', function (req, res) {
+    res.statusCode = 404;
+    res.sendFile(path.join(__dirname + "/src/error404.html"));
+})
+
+app.use(function (req, res, next) {
+     res.redirect('/404.html');
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
