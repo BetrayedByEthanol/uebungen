ab3f58d52dd95f5ee19d7db5420e613f8275b5ba
Denis Heimburger <d.heimburger@albbw.de>
2019-12-20 08:05:18 +0100

    #WebGrundlagenB003: index.html angepasst, app.js refaktoriert

--- web/htmlBasics/WebGrundlagenb001/app.js
+++ web/htmlBasics/WebGrundlagenb001/app.js
@@ -2,17 +2,12 @@ const express = require("express");
 var path = require("path");
 const app = express();
 const port = 5050;
-//Static file server bla bla
-app.use(express.static("src"));
-app.set("view engine", "pug");
 
-app.get('/404.html', function (req, res) {
-    res.statusCode = 404;
-    res.sendFile(path.join(__dirname + "/src/error404.html"));
-})
+app.use(express.static("src"));
 
 app.use(function (req, res, next) {
-     res.redirect('/404.html');
+    res.statusCode = 404;
+    res.sendFile(path.join(__dirname + "/src/error404.html"));
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
