57a6b06635da4f507449d80d2f7530ec5008fd17
Cody <artificialfxx@gmail.com>
2020-01-09 15:33:17 +0100

    Quotes

--- web/htmlBasics/WebGrundlagenb001/src/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/index.js
@@ -9,10 +9,15 @@ var wennGeladen = function () {
 
     zitatElement.innerText = quotes[randomIndex].quote;
 
-    const ausbilderquote = quotes.find(quote => quote.author.age == 'ausbilderquote');
+    const ausbilderquote = quotes.find(quote => quote.id == 'ausbilderquote');
     
     const ausbilderquoteElement = document.getElementById('ausbilderquote');
     const ausbilderquoteAuthor = document.getElementById('ausbilderquoteAuthor');
+
+    const zechquote = quotes.find(quote => quote.author == 'zechquote');
+
+    const zechquoteElement = document.getElementById('zechquote');
+    const zechquoteAuthor = document.getElementById('zechquoteAuthor');
 
     ausbilderquoteElement.innerText = ausbilderquote.quote;
     ausbilderquoteAuthor.innerText = ausbilderquote.author.name;
