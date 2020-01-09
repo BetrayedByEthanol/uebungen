4fa8a78b5fb3a37827be670786d267642d7d00a4
19nieke <JS_Ram38@hotmail.com>
2020-01-09 15:11:50 +0100

    moved Quote to json

--- web/htmlBasics/WebGrundlagenb001/src/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/index.js
@@ -9,12 +9,18 @@ var wennGeladen = function () {
     zitat.innerText = quotes[0].quote;
 
     const ausbilderquote = quotes.find(quote => quote.id == 'ausbilderquote');
+    const niekequote = quotes.find(quote => quote.id == '19niekequote');
 
     const ausbilderquoteElement = document.getElementById('ausbilderquote');
     const ausbilderquoteAuthor = document.getElementById('ausbilderquoteAuthor');
-    
+    const niekequoteElement = document.getElementById('19niekequote');
+    const niekequoteAuthor = document.getElementById('19niekeAuthor');    
+
     ausbilderquoteElement.innerText = ausbilderquote.quote;
     ausbilderquoteAuthor.innerText = ausbilderquote.author.name;
+    niekequoteElement.innerText = niekequote.quote;
+    niekequoteAuthor.innerText = niekequote.author.name;
+
 }
 
 const http = new XMLHttpRequest();
