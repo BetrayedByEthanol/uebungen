4fbffd3d3e15a6ffd63a3ce6cae1731414fafa2a
Cody <artificialfxx@gmail.com>
2020-01-09 15:41:51 +0100

    Bug fix

--- web/htmlBasics/WebGrundlagenb001/src/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/index.js
@@ -9,18 +9,15 @@ var wennGeladen = function () {
     zitatElement.innerText = quotes[randomIndex].quote;
 
     const ausbilderquote = quotes.find(quote => quote.id == 'ausbilderquote');
-    const niekequote = quotes.find(quote => quote.id == '19niekWequote');
+    const niekequote = quotes.find(quote => quote.id == '19niekequote');
     const zechquote = quotes.find(quote => quote.author == 'zechquote');
-    
-
-    const ausbilderquote = quotes.find(quote => quote.id == 'ausbilderquote');
     
     const ausbilderquoteElement = document.getElementById('ausbilderquote');
     const ausbilderquoteAuthor = document.getElementById('ausbilderquoteAuthor');
     const niekequoteElement = document.getElementById('19niekequote');
     const niekequoteAuthor = document.getElementById('19niekeAuthor');    
-    const zechquoteElement = document.getElementById('zechquote');
-    const zechquoteAuthor = document.getElementById('zechquoteAuthor');
+    const zechquoteElement = document.getElementById('19zechquote');
+    const zechquoteAuthor = document.getElementById('19zechquoteAuthor');
 
     ausbilderquoteElement.innerText = ausbilderquote.quote;
     ausbilderquoteAuthor.innerText = ausbilderquote.author.name;
