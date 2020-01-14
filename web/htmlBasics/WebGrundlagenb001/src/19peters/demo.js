496c45f5c700e2dbfa4eacbdc44c061218f6ed18
19peters <Benjamin.Peters@aloberlin.onmicrosoft.com>
2020-01-14 11:05:18 +0100

    demo.html, demo.js added

---
+++ web/htmlBasics/WebGrundlagenb001/src/19peters/demo.js
@@ -0,0 +1,5 @@
+function open_cont(num) {
+    var conta = document.getElementsByClassName("cont")[num];
+    if (conta.style.display == "none") {conta.style.display = "block"}
+    else {conta.style.display = "none"}
+}
