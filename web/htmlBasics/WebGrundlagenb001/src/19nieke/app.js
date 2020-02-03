09417c6700f5c4f5fac4a27a3f69625aec791e2e
19nieke <JS_Ram38@hotmail.com>
2020-02-03 09:26:45 +0100

    OS rename field ipaddress to votes

---
+++ web/htmlBasics/WebGrundlagenb001/src/19nieke/app.js
@@ -0,0 +1,21 @@
+const url = 'mongodb://10.42.53.5:27017';
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db('FIAN19-II');
+    dbo.collection('obliquestrategies').find({}).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        result.forEach(p => {
+            var myQuery = { _id: p._id };
+            var newValues = { $rename: { upvotes: 0, downvotes: 0, ipAddress: [] } }
+            dbo.collection('obliquestrategies').updateOne(myQuery, {$rename: {'ipAddress' : 'votes'}}, function (err, res) {
+                if (err) throw err;
+                console.log(res);
+            });
+        });
+     db.close();   
+    });
+});
+
