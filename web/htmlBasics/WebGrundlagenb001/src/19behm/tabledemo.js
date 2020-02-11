35f939045ddbdf50603a91ac038e4bd17ed5c89c
19behm <ju.behm@gmail.com>
2020-02-11 09:59:58 +0100

    Updated Filter (RC)

--- web/htmlBasics/WebGrundlagenb001/src/19behm/tabledemo.js
+++ web/htmlBasics/WebGrundlagenb001/src/19behm/tabledemo.js
@@ -37,17 +37,21 @@ function sortTable(n) {
 }
 
 function filterTable() {
-    var input , filter, table, tr, td,i,txtValue;
+    var input , filter, table, tr, td1,td2,td3,i,txtValue1,txtValue2,txtValue3;
     input = document.getElementById("searchinput");
     filter = input.value.toUpperCase();
     table = document.getElementById("quotetable");
     tr = table.getElementsByTagName("tr");
 
     for (i=0;i<tr.length;i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if(td) {
-            txtValue = td.textContent || td.innerText;
-            if(txtValue.toUpperCase().indexOf(filter) > -1){
+        td1 = tr[i].getElementsByTagName("td")[0];
+        td2 = tr[i].getElementsByTagName("td")[1];
+        td3 = tr[i].getElementsByTagName("td")[2];
+        if(td1) {
+            txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
+            if(txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter)> -1 || txtValue3.toUpperCase().indexOf(filter) > -1){
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
