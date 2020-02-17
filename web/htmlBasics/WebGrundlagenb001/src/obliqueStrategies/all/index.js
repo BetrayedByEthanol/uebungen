5d5bbb5ca8435b4c2dede802875d868d395e697e
Denis Heimburger <d.heimburger@albbw.de>
2020-02-17 15:05:39 +0100

    Oblique strategies All. vue.js implementation

---
+++ web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/all/index.js
@@ -0,0 +1,32 @@
+
+Vue.component('o-card', {
+
+    template: `
+    <div class="card" >
+    <h5 class="card-title">{{strategy.phrase}}</h5>
+    <p class="align-text-right">{{strategy.category}}</p>
+    
+    </div>`,
+    props: ['strategy']
+});
+
+var app = new Vue({
+
+    el: '#app',
+    data: {
+        strategies: []
+    },
+    methods: {
+        sortiere: function () {
+            this.strategies.sort(function (a, b) {
+                if (a.phrase > b.phrase) return 1;
+                if (b.phrase > a.phrase) return -1;
+            })
+        }
+    },
+    mounted() {
+        axios
+            .get('/strategies')
+            .then(response => (this.strategies = response.data));
+    }
+});
