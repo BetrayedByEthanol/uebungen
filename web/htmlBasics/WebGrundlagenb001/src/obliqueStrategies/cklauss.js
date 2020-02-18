58439005d6d75386d01edcc0d77710a15da43557
Felix <felix.t@wunderbar.de>
2020-02-18 09:57:45 +0100

    Change Color Funtion added

--- web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/cklauss.js
+++ web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/cklauss.js
@@ -17,7 +17,7 @@ var wennGeladen = function () {
     }
     phrases = allPhrases;
     refresh();
-
+    
 }
 
 function toggler() {
@@ -42,14 +42,57 @@ function refresh() {
             currentPhraseID = phrases[randomNumber].id;
             currentPhraseMongoID = phrases[randomNumber]._id;
             document.getElementById('category').innerText = phrases[randomNumber].category;
-            getRating();
+            ColorChange();
+            getRating();          
         }
     }
+   
     var setWait = window.setInterval(wait, 40);
+    
 }
 
 function filterPhrases(category) {
     phrases = allPhrases.filter(p => { return p.category == category });
+}
+
+function ColorChange(){
+    const theme = document.getElementById("category").innerText;
+    if(theme == "Growth"){
+        document.getElementById("jumbo").className = "jumbotron bg-success mh-100";
+        document.getElementById("content").className = "display-5 text-break text-light";
+        document.getElementById("category").className = "lead text-right text-light";
+    }
+    else if(theme == "New Perspective"|| theme == "Self-Care,New Perspective,Philosophical"){
+        document.getElementById("jumbo").className = "jumbotron bg-secondary mh-100";
+        document.getElementById("content").className = "display-5 text-break text-light";
+        document.getElementById("category").className = "lead text-right text-light";  
+    }
+    else if(theme =="Programming" ||theme == "Philosophy" || theme == "Philosophical" || theme == "Philosophical,Programming,Self-Confidence,New Perspective"){
+        document.getElementById("jumbo").className = "jumbotron bg-warning mh-100";
+        document.getElementById("content").className = "display-5 text-break text-dark";
+        document.getElementById("category").className = "lead text-right text-dark";
+    }
+    else if(theme == "Technology" || theme == "Technology,New Perspective"){
+        document.getElementById("jumbo").className = "jumbotron bg-info mh-100";
+        document.getElementById("content").className = "display-5 text-break text-light";
+        document.getElementById("category").className = "lead text-right text-light";
+    }
+    else if(theme == "Self-Confidence" || theme == "Self-Care" ){
+        document.getElementById("jumbo").className = "jumbotron bg-danger mh-100";
+        document.getElementById("content").className = "display-5 text-break text-light";
+        document.getElementById("category").className = "lead text-right text-light";
+    }
+    else if(theme == "Technology"){
+        document.getElementById("jumbo").className = "jumbotron bg-light mh-100";
+        document.getElementById("content").className = "display-5 text-break text-dark";
+        document.getElementById("category").className = "lead text-right text-dark";
+    }
+    else
+    {
+        document.getElementById("jumbo").className = "jumbotron bg-dark mh-100";
+        document.getElementById("content").className = "display-5 text-break text-light";
+        document.getElementById("category").className = "lead text-right text-light";
+    }
 }
 
 
