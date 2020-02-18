68aba19514690973974b5b78b0ae50f300a9da10
Nils <Doodlegante@mehrfabigerhalbkreisfoermigerbogen.de>
2020-02-18 14:47:59 +0100

    filter-feature work in progress, Nils Rose

--- web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/all/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/all/index.js
@@ -30,3 +30,35 @@ var app = new Vue({
             .then(response => (this.strategies = response.data));
     }
 });
+
+var filterlist = function () {
+    while(document.getElementById('Kategorieliste').hasChildNodes == true) {
+        document.getElementById('Kategorieliste').firstChild.remove();
+    }
+    document.getElementById('Kategorieliste').hidden = null;
+    const liste = ['Alle Kategorien','Growth','New Perspective','Programming','Philosophical','Self-Care','Self-Confidence','Technology'];
+    liste.sort();
+    liste.forEach(eintrag => {
+        if(document.getElementById('Kategoriefeld').innerText != eintrag) {
+            var newdiv = document.createElement('div');
+            document.getElementById('Kategorieliste').append(newdiv);
+            newdiv.id = 'Kategorie' + liste.indexOf(eintrag);
+            newdiv.innerText = eintrag;
+            newdiv.setAttribute('onclick','filter("' + eintrag + '")');
+        }
+    });
+};
+
+var filter = function (choose) {
+    document.getElementById('Kategorieliste').hidden = true;
+    const carddeck = document.querySelectorAll('[classname="card"]');
+    carddeck.forEach(card => {
+        card.hidden = true;
+    });
+    carddeck.forEach(card => {
+        if(card.getElementsByTagName('p').innerText.includes(choose)) {
+            card.hidden = false;
+        }
+    })
+    document.getElementById('Kategoriefeld').innerText = choose
+};
