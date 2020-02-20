a893560a5d19284b1d2e030578898494b3e03702
19behm <ju.behm@gmail.com>
2020-02-20 12:31:29 +0100

    added placeholder page for id based OS

---
+++ web/htmlBasics/WebGrundlagenb001/src/obliqueStrategies/os-id.js
@@ -0,0 +1,21 @@
+Vue.component('ocardid', {
+    template: `
+    <div class="jumbotron bg-primary" >
+    <h5 class="display-4 text-center">{{strategy.phrase}}</h5>
+    <p class="text-right">{{strategy.category.join(', ')}}</p>
+    </div>`,
+
+    props: ['strategy']
+})
+
+var app = new Vue({
+        el: '#app',
+        data: {
+            strategy: []
+        },
+        mounted() {
+            axios
+                .get('/strategies/5e2ffe4873c7467d98267cc1')
+                .then(response => (this.strategy = response.data));
+        }
+    })
