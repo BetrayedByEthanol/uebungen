40f91cafdea1e6e295c69d3c0bedfa5c39b5d45a
Cody <artificialfxx@gmail.com>
2020-02-24 15:05:49 +0100

    SSL instructions updated

--- web/htmlBasics/WebGrundlagenb001/app.js
+++ web/htmlBasics/WebGrundlagenb001/app.js
@@ -139,7 +139,7 @@ client.connect().then((client) => {
     //Create the server using an SSL connection - Need to use HTTPS:// noww
     https.createServer({
         //Server Key and Certificate as well as passphrase
-        //Renew Self Signed key and certificate after 90 days with `openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 90` in Terminal
+        //Renew Self Signed key and certificate after 90 days with `openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 90` in Terminal and delete old ones
         key: fs.readFileSync('./key.pem'),
         cert: fs.readFileSync('./cert.pem'),
         passphrase: 'FIAN19'
