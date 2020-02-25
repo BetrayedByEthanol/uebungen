d579e2343d45bba307e0550ae0f9c2731df6a398
Felix <felix.t@wunderbar.de>
2020-02-25 15:19:58 +0100

    Task Component

--- web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
@@ -1,5 +1,3 @@
-
-
 Vue.component('dtt-spalte', {
     template: `<div class='list-group-item w-100 p-0'>
     <div class='text-center m-3'>{{schoof.zeile}}</div>
@@ -10,12 +8,36 @@ Vue.component('dtt-spalte', {
     props: ['schoof']
 });
 
-Vue.component('dtt-task', {
+
+Vue.component('dtt-task',{
+    template: `
+    <div class="card" style="max-width: 400px;">
+    <div class="card-header">
+        <a href="#" class="btn btn-success btn-sm">P</a> <a href="#" class="btn btn-info btn-sm">JS</a> <a
+            href="#" class="btn btn-primary btn-sm">Vue</a> 
+            <a href="#" class="btn btn-danger btn-sm">OK</a>
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">TaskHeader</h5>
+        <p class="card-text">(Optional)Lerne Vue!</p>
+        <p class="card-text">Schreibe etwas Quelltext!</p>
+        <p class="card-text">Behebe Programmierfehler</p>
+        <a href="#" class="btn btn-primary btn-sm"><=</a>
+        <a href="#" class="btn btn-primary btn-sm">=></a>
+    </div>
+</div>`,
+props: ['gans']
+    
+})
+
+
+
+/*Vue.component('dtt-task', {
     template: `
     <div>{{gans}}</div>
     `,
     props: ['gans']
-})
+})*/
 
 var module = new Vue({
     el: '#app',
