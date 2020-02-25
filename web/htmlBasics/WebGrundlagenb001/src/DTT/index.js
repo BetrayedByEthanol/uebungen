769e6961a4c031dfdf4fee1b02023914621ceb32
Nils <Doodlegante@mehrfabigerhalbkreisfoermigerbogen.de>
2020-02-25 12:33:40 +0100

    added vue component 'Spalten' for qc, Nils Rose

--- web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
@@ -1,9 +1,19 @@
-
+Vue.component('Spalten', {
+    template: `<div class='list-group-item w-100 p-0'>
+    <div class='text-center m-3'>fillertitle</div>
+    <div class='list-group list-group-flush border'>
+        <div class='list-group-item'>...filler1...</div>
+        <div class='list-group-item'>...filler2...</div>
+        <div class='list-group-item'>...filler3...</div>
+        <div class='list-group-item'>...filler4...</div>
+    </div></div>`,
+    props: ['tec']
+});
 
 var module = new Vue({
     el: '#ape',
     data: {
-
+        test: [1,2,3,4,5],
     },
     methods: {
 
