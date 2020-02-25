a713300dcd5c41ef793ba2d087afbb130e6311b2
Nils <Doodlegante@mehrfabigerhalbkreisfoermigerbogen.de>
2020-02-25 12:55:08 +0100

    Anfänge von Vue component 'Einzeltask', Nils Rose

--- web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
+++ web/htmlBasics/WebGrundlagenb001/src/DTT/index.js
@@ -2,18 +2,23 @@ Vue.component('Spalten', {
     template: `<div class='list-group-item w-100 p-0'>
     <div class='text-center m-3'>fillertitle</div>
     <div class='list-group list-group-flush border'>
-        <div class='list-group-item'>...filler1...</div>
-        <div class='list-group-item'>...filler2...</div>
-        <div class='list-group-item'>...filler3...</div>
-        <div class='list-group-item'>...filler4...</div>
+        <Einzeltask v-bind:gans='gans' v-for='gans in schoof' class='list-group-item'></Einzeltask>
+        
     </div></div>`,
-    props: ['tec']
+    props: ['schoof']
 });
+
+Vue.component('Einzeltask', {
+    template: `
+    <div>{{gans}}</div>
+    `,
+    props: ['gans']
+})
 
 var module = new Vue({
     el: '#ape',
     data: {
-        test: [1,2,3,4,5],
+        wasser: [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]],
     },
     methods: {
 
